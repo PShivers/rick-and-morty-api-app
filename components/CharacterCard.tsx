7e7c37ab641e3d5f4f848e74e25f818384bc4c60
PShivers <paulsshivers@gmail.com>
2023-07-17 12:50:37 -0400

    changed styling of character cards

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -7,10 +7,13 @@ interface CharacterCardProps {
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
   return (
-    <div className="m-1">
-      <img src={character.image} alt={character.name} />
-      <h3>{character.name}</h3>
-    </div>
+    <div className="max-w-xs rounded overflow-hidden shadow-lg m-1"  style={{maxWidth:300}} >
+      <img className="w-full" src={character.image} alt={character.name}/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-center text-xl mb-2 truncate">{character.name}</div>
+
+      </div>
+   </div>
   );
 };
 
