04d85feff4bebaab67011fe76d2c635360a8529b
PShivers <paulsshivers@gmail.com>
2023-07-17 16:21:24 -0400

    added max width so text would truncate

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -9,12 +9,12 @@ interface CharacterCardProps {
 const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
   return (
     <Link href={`/characters/${character.id}`}>
-        <div className="max-w-xs rounded overflow-hidden shadow-lg m-1" >
+      <div className="max-w-xs rounded overflow-hidden shadow-lg m-1" style={{maxWidth:300}}>
         <img className="w-full" src={character.image} alt={character.name}/>
         <div className="px-6 py-4">
           <div className="font-bold text-center text-xl mb-2 truncate">{character.name}</div>
         </div>
-   </div>
+      </div>
     </Link>
 
   );
