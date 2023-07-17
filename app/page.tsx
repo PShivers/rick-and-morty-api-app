5a8c597cfa37706449db540fc1c5de65b82b65e2
PShivers <paulsshivers@gmail.com>
2023-07-17 11:54:32 -0400

    imported/implemented character type for cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useQuery, gql } from '@apollo/client';
 import apolloClient from '../apollo-client';
+import { Character } from '../types';
+
 
 const CHARACTERS_QUERY = gql`
   query {
@@ -33,7 +35,7 @@ export default function Page() {
     <div className='flex flex-col justify-center items-center w-full '>
       <h1 className={'text-3xl my-5'}>Rick and Morty Characters</h1>
       <div className='flex flex-wrap justify-center items-center'>
-      {characters.map((character) => (
+      {characters.map((character:Character) => (
         <div key={character.id} className='m-1'>
           <img src={character.image} alt={character.name} />
           <h3>{character.name}</h3>
